09ed834565c6876cd8afdec85062295902c11b4d
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-23 22:40:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   new_collection =[]
   i = 0
   while i < collection.length
-    new_collection << yield(collection[i]) == true
+    yield(collection[i]) == true
     i +=  1
   end
   new_collection
